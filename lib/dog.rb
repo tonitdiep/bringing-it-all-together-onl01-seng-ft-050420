76a2634c9792e2ee235ee5c473c6d27a3a50cdaf
Toni  Diep <adiep123@gmail.com>
2020-06-13 21:32:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,7 +73,7 @@ class Dog
       if !dog.empty?
         dog_data = dog[0]
         dog = new_from_db(dog_data)
-        dog = Dog.new(id: attr_hash[0], name: attr_hash[1], breed: attr_hash[2])
+        # dog = Dog.new(id: attr_hash[0], name: attr_hash[1], breed: attr_hash[2])
       else
         dog = self.create(name: name, breed: breed)
       end
